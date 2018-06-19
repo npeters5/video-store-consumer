78f0ffa5707a01b0f26eed77de936551cdfd12e7
Nora Peters <norapeters5@gmail.com>
2018-06-19 11:25:00 -0700

    adds Video component

---
+++ src/components/Video.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Video.css';
+
+class Video extends Component {
+
+  // onDeleteClick = (event) => {
+  //   console.log(event.target.id);
+  //   event.preventDefault();
+  //   this.props.removeVideo(event.target.id);
+  // }
+
+  render() {
+
+    return (
+      <div>
+        {this.props.title}
+      </div>
+    )
+  }
+}
+
+// Video.propTypes = {
+//
+// };
+
+export default Video;
